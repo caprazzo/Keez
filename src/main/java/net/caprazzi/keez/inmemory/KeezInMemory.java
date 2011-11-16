f94c1bd59c1a650d50f016d450e47d315aafc33d
Matteo Caprari <matteo.caprari@gmail.com>
2011-11-16 16:53:02 +0000

    added synchronization to KeezInMemory

--- src/main/java/net/caprazzi/keez/inmemory/KeezInMemory.java
+++ src/main/java/net/caprazzi/keez/inmemory/KeezInMemory.java
@@ -8,6 +8,7 @@ import static net.caprazzi.keez.Helpers.notFound;
 import static net.caprazzi.keez.Helpers.ok;
 import static net.caprazzi.keez.Helpers.notNull;
 
+import java.util.LinkedList;
 import java.util.Set;
 import java.util.SortedSet;
 
@@ -47,39 +48,40 @@ public class KeezInMemory implements Keez.Db {
 		notNull(callback);
 		
 		try {
-			
 			if (rev == 0) {
 				create(key, body, callback);
 				return;
 			}
 			
-			SortedSet<Value> revisions = data.get(key);
-			if (revisions.size() == 0) {
-				callback.collision(key, rev, -1);
+			Value value = null;
+			Value lastRevision = null;
+			synchronized (data) {
+				lastRevision = getLastRevision(key);
+				if (lastRevision != null && rev == lastRevision.rev) {
+					int newRevision = lastRevision.rev + 1;	
+					value = new Value(newRevision, body);
+					data.put(key, value);			
+					if (autoPurge) {
+						purgeOldRevisions(key, newRevision);
+					}
+				}
+			}
+			
+			if (value != null) {
+				ok(callback, key, value.rev);
 				return;
 			}
 			
-			int lastRevision = getRevision(revisions);
-			
-			if (rev != lastRevision) {
-				collision(callback, key, rev, lastRevision);
-				return;
+			if (lastRevision != null) {
+				collision(callback, key, rev, lastRevision.rev);
 			}
-			
-			int newRevision = lastRevision + 1;
-			
-			data.put(key, new Value(newRevision, body));
-			
-			if (autoPurge) {
-				purgeOldRevisions(key, newRevision);
+			else {
+				collision(callback, key, rev, -1);
 			}
-		
-			ok(callback, key, newRevision);
 		}
 		catch (Exception e) {
 			callback.error(key, e);
 		}
-		
 	}
 	
 	@Override
@@ -88,16 +90,15 @@ public class KeezInMemory implements Keez.Db {
 		notNull(callback);
 		
 		try {
-			SortedSet<Value> revisions = data.get(key);
-			if (revisions.size() == 0) {
+			Value value = getLastRevision(key);
+			if (value == null) {
 				notFound(callback, key);
 				return;
 			}
 			
-			Value value = revisions.last();
 			int revision = value.rev;
 			byte[] body = value.body;
-			found(callback, key, revision, body);			
+			found(callback, key, revision, body);		
 		}
 		catch (Exception e) {
 			callback.error(key, e);
@@ -110,15 +111,22 @@ public class KeezInMemory implements Keez.Db {
 		notNull(callback);
 		
 		try {
+			
+			Value value = null;
 			SortedSet<Value> revisions = data.get(key);
-			if (revisions.size() == 0) {
-				notFound(callback, key);
-				return;
+			synchronized (data) {
+				if (revisions.size() > 0) {
+					value = revisions.last();
+					data.removeAll(key);
+				}
 			}
 			
-			Value value = revisions.last();
-			data.removeAll(key);
-			deleted(callback, key, value.body);
+			if (value == null) {
+				notFound(callback, key);
+			}
+			else {
+				deleted(callback, key, value.body);	
+			}			
 		}
 		catch (Exception e) {
 			callback.error(key, e);
@@ -129,19 +137,22 @@ public class KeezInMemory implements Keez.Db {
 	public void list(List callback) {
 		notNull(callback);
 		
-		try {
+		try {			
+			LinkedList<Entry> entries = new LinkedList<Keez.Entry>();
 			Set<String> keys = data.keySet();
-			if (keys.size() == 0) {
+			synchronized (data) {
+				for(String key : keys) {
+					Value v = data.get(key).last();
+					entries.add(new Entry(key, v.rev, v.body));
+				}
+			}
+			
+			if (entries.size() == 0) {
 				notFound(callback);
 				return;
 			}
-			entries(callback, Iterables.transform(keys, new Function<String, Entry>() {
-				@Override
-				public Entry apply(String key) {
-					Value v = data.get(key).last();
-					return new Entry(key, v.rev, v.body);
-				}
-			}));
+			
+			entries(callback, entries);
 		}
 		catch(Exception e) {
 			callback.error(e);
@@ -154,23 +165,24 @@ public class KeezInMemory implements Keez.Db {
 		notNull(callback);
 		
 		try {
+			LinkedList<Entry> entries = new LinkedList<Keez.Entry>();
 			SortedSet<Value> revisions = data.get(key);
-			if (revisions.size() == 0) {
+			synchronized (data) {
+				for(Value v : revisions) {
+					entries.add(new Entry(key, v.rev, v.body));
+				}
+			}
+			
+			if (entries.size() == 0) {
 				notFound(callback, key);
 				return;
 			}
 			
-			found(callback, key, Iterables.transform(revisions, new Function<Value, Entry>() {
-				@Override
-				public Entry apply(Value value) {
-					return new Entry(key, value.rev, value.body);
-				}
-			}));
+			found(callback, key, entries);
 		}
 		catch(Exception e) {
 			callback.error(key, e);
 		}
-		
 	}
 
 	private void purgeOldRevisions(String key, int newRevision) {
@@ -178,24 +190,42 @@ public class KeezInMemory implements Keez.Db {
 		SortedSet<Value> toRemove = revisions.subSet(revisions.first(), revisions.last());
 		revisions.removeAll(toRemove);
 	}
+	
+	/**
+	 * Puts key:value if the key does not exist, otherwise returns the latest revision for that key
+	 * @param key
+	 * @param value
+	 * @return the latest revision for this key if the key is already present, otherwise null
+	 */
+	private Value putIfNotFound(String key, Value value) {
+		synchronized (data) {			
+			if (data.containsKey(key)) {
+				return data.get(key).last();
+			}	
+			data.put(key, value);
+			return null;
+		}
+	}
 
 	private void create(String key, byte[] body, Put callback) {
-		SortedSet<Value> revisions = data.get(key);
-		if (revisions.size() > 0) {
-			int foundRev = getRevision(revisions);
-			collision(callback, key, 0, foundRev);
-			return;
+		Value value = new Value(1, body);
+		Value found = putIfNotFound(key, value);
+		if (found == null) {
+			ok(callback, key, value.rev);
 		}
-		
-		Value v = new Value(1, body);
-		data.put(key, v);
-		int revision = v.rev;
-		
-		ok(callback, key, revision);		
+		else {
+			collision(callback, key, 0, found.rev);
+		}		
 	}
 	
-	private int getRevision(SortedSet<Value> revisions) {
-		return revisions.last().rev;		
+	private Value getLastRevision(String key) {
+		synchronized (data) {
+			SortedSet<Value> revisions = data.get(key);
+			if (revisions.size() > 0) {
+				return revisions.last();
+			}			
+		}
+		return null;
 	}
 
 	private static class Value  implements Comparable<Value> {
